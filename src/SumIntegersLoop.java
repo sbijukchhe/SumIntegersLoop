2c44c088adb4b8bdbf459cc029073c87df9d21ca
GBTC441013UR <GBTC441013UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-09-05 14:18:42 -0400

    Adding numbers using for loop

---
+++ src/SumIntegersLoop.java
@@ -0,0 +1,43 @@
+/*
+The following program should allow the users to input 10 integers and output the total. Please finish it.
+
+import java.util.Scanner;
+
+public class GetIntegers {
+
+    public void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+
+        int num, total = 0;
+
+        // Loop should start here
+        num = keyboard.nextInt();
+        total += num;
+        // Loop should end here
+
+        System.out.println("The total of all 10 numbers is " + total);
+
+    }
+
+}
+ */
+
+import java.util.Scanner;
+
+public class SumIntegersLoop {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        int num, total = 0;
+
+        System.out.println("Please enter 10 numbers to find sum...");
+
+        for(int i=0; i<=10; i++){
+
+            num = input.nextInt();
+            total += num;
+        }
+        System.out.println("\nThe total of all 10 numbers is " + total);
+    }
+}
